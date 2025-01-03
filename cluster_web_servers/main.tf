3cc99afbe9d7136e780b2c1aa9a52c41c9277122
David Eliason <davethemaker@gmail.com>
2025-01-03 09:19:23 -0800

    add ASG resource

--- cluster_web_servers/main.tf
+++ cluster_web_servers/main.tf
@@ -38,3 +38,18 @@ resource "aws_launch_configuration" "example" {
 		systemctl enable httpd
 		EOF
 }
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  
+  min_size		= 2
+  max_size		= 10
+
+  tag {
+    key			= "Name"
+    value		= "terraform-asg-example"
+    propogate_at_launch	= true
+  }
+}
+
+
