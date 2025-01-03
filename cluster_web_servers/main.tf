e89a08f2093f838cb3756b7524d2be3f878bf268
David Eliason <davethemaker@gmail.com>
2025-01-03 08:29:32 -0800

    add sg

--- cluster_web_servers/main.tf
+++ cluster_web_servers/main.tf
@@ -13,3 +13,15 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_security_group" "instance" {
+  name = var.instance_security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
