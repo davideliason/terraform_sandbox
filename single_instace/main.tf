bbb1021d18fbf602fcec997043c7dc6c963dfd81
David Eliason <davethemaker@gmail.com>
2025-01-02 20:28:35 -0800

    added a reference to the security group to the aws_instance resource to allow ingress to port 8080

--- single_instace/main.tf
+++ single_instace/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-07d9cf938edb0739b"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
 		#!/bin/bash
 		echo "hello world!" > index.html
