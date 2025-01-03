123e20bbf33630bffd665c3d45a7bc6454937474
David Eliason <davethemaker@gmail.com>
2025-01-02 20:16:13 -0800

    add instance resource with tags, successfully deployed

--- single_instace/main.tf
+++ single_instace/main.tf
@@ -2,4 +2,13 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_instance" "example" {
+  ami = "ami-07d9cf938edb0739b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
 
