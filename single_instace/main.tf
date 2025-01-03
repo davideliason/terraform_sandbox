260422970c3f56e043ead63aa4a9a618b94296cc
David Eliason <davethemaker@gmail.com>
2025-01-02 20:21:26 -0800

    add sg to allow access to the ec2 instance on port 8080 tcp

--- single_instace/main.tf
+++ single_instace/main.tf
@@ -6,8 +6,27 @@ resource "aws_instance" "example" {
   ami = "ami-07d9cf938edb0739b"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+		#!/bin/bash
+		echo "hello world!" > index.html
+		nohup busybox httpd -f -p 8080 &
+		EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     Name = "tf-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "tf-example-instance"
+
+  ingress {
+    from_port 	= 8080
+    to_port 	= 8080
+    protocolo	= "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
   }
 }
 
